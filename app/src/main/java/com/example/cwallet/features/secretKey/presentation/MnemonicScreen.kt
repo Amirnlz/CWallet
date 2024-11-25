e3508ce838ac85920994ba2d19eb54c5734724e6
Amir <amiirnlz79@gmail.com>
2024-11-26 01:00:44 +0330

    Revise UI of Mnemonic-screen

---
+++ app/src/main/java/com/example/cwallet/features/secretKey/presentation/MnemonicScreen.kt
@@ -0,0 +1,108 @@
+package com.example.cwallet.features.secretKey.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.cwallet.R
+import com.example.cwallet.ui.theme.CWalletTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MnemonicScreen(modifier: Modifier = Modifier, viewModel: SecretKeyViewModel) {
+
+    val mnemonicWords by viewModel.secretKeys.collectAsStateWithLifecycle()
+
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = "Mnemonic Phrase") })
+    }) { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Text(
+                text = stringResource(R.string.saveMnemonicWords),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(16.dp)
+            )
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(mnemonicWords.keys.withIndex().toList()) { (index, word) ->
+                    SecretKeyWordItem(word = word, index = index)
+                }
+            }
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = stringResource(R.string.i_ve_written_it_down))
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SecretKeyWordItem(
+    modifier: Modifier = Modifier,
+    word: String,
+    index: Int,
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = MaterialTheme.shapes.medium
+            )
+            .padding(12.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "${index + 1}. $word",
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+
+@PreviewLightDark
+@Composable
+private fun PreviewWordBox() {
+    CWalletTheme {
+        SecretKeyWordItem(word = "Sky", index = 2)
+    }
+}

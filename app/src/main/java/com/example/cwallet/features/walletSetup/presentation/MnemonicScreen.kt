5f0e3e8131bb1fd0fda7e579541e5b018d06c026
Amir <amiirnlz79@gmail.com>
2024-12-02 13:28:50 +0330

    Change UI of mnemonic words

--- app/src/main/java/com/example/cwallet/features/walletSetup/presentation/MnemonicScreen.kt
+++ app/src/main/java/com/example/cwallet/features/walletSetup/presentation/MnemonicScreen.kt
@@ -1,16 +1,21 @@
 package com.example.cwallet.features.walletSetup.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -21,11 +26,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.cwallet.R
 import com.example.cwallet.ui.theme.CWalletTheme
@@ -61,7 +68,7 @@ fun MnemonicScreen(
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 items(mnemonicWords.words.withIndex().toList()) { (index, word) ->
-                    SecretKeyWordItem(word = word, index = index)
+                    WordItem(word = word, index = index)
                 }
             }
             Button(
@@ -79,26 +86,52 @@ fun MnemonicScreen(
 
 
 @Composable
-fun SecretKeyWordItem(
+fun WordItem(
     modifier: Modifier = Modifier,
     word: String,
     index: Int,
 ) {
-    Box(
+    Row(
         modifier = modifier
-            .background(
-                color = MaterialTheme.colorScheme.surfaceVariant,
-                shape = MaterialTheme.shapes.medium
-            )
-            .padding(12.dp),
-        contentAlignment = Alignment.Center
+            .clip(RoundedCornerShape(8.dp))
+            .border(1.dp, MaterialTheme.colorScheme.surfaceDim, RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.surfaceVariant),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = "${index + 1}. $word",
-            style = MaterialTheme.typography.bodyMedium,
-            fontWeight = FontWeight.SemiBold,
-            textAlign = TextAlign.Center
+        // Number section
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+                .width(20.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = (index + 1).toString(),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+        }
+
+        // Vertical Divider
+        Box(
+            modifier = Modifier
+                .width(1.dp)
+                .height(24.dp)
+                .background(MaterialTheme.colorScheme.surfaceDim)
         )
+
+        // Value section
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+        ) {
+            Text(
+                text = word,
+                fontSize = 14.sp,
+                color = Color.Black
+            )
+        }
     }
 }
 
@@ -107,6 +140,6 @@ fun SecretKeyWordItem(
 @Composable
 private fun PreviewWordBox() {
     CWalletTheme {
-        SecretKeyWordItem(word = "Sky", index = 2)
+        WordItem(word = "Sky", index = 15)
     }
 }

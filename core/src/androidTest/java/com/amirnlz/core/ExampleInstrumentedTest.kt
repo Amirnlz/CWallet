6a65dced20a318e04e097a59b09c3d9b32599801
Amir <amiirnlz79@gmail.com>
2024-12-03 11:14:22 +0330

    Fix: core module template to Android Library

--- core/src/androidTest/java/com/amirnlz/core/ExampleInstrumentedTest.kt
+++ core/src/androidTest/java/com/amirnlz/core/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.amirnlz.core", appContext.packageName)
+        assertEquals("com.amirnlz.core.test", appContext.packageName)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,5 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "CWallet"
 include(":app")
-include(":core")
 include(":wallet")
+include(":core")

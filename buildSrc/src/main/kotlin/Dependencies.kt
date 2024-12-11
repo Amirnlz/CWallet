74b4f6d9a408e8b5fdb4828e55ee946e375391ca
Amir <amiirnlz79@gmail.com>
2024-12-12 01:33:31 +0330

    Chore: add dependency handlers

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,9 +1,59 @@
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
 object Dependencies {
-    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    // Compose Dependencies
+    const val composeMaterial = "androidx.compose.material3:material3:${Versions.composeMaterial3}"
     const val composeUi = "androidx.compose.ui:ui:${Versions.compose}"
-    const val hilt = "com.google.dagger:hilt-android:${Versions.hilt}"
+    const val composeRuntime = "androidx.compose.runtime:runtime:${Versions.compose}"
+    const val composeUiGraphics = "androidx.compose.ui:ui-graphics:${Versions.compose}"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+    const val composeUiToolingPreview = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+
+
+    // Hilt Dependencies
+    const val hiltAndroid = "com.google.dagger:hilt-android:${Versions.hilt}"
+    const val hiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+
+    // Retrofit Dependencies
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val moshiConverter = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
+    const val okhttpLoggingInterceptor =
+        "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+
+    // Room Dependencies
+    const val roomRuntime = "androidx.room:room-runtime:${Versions.room}"
+    const val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+    const val roomKtx = "androidx.room:room-ktx:${Versions.room}"
 }
 
-object TestDependencies {
-    const val junit = "junit:junit:4.13.2"
+fun DependencyHandler.compose() {
+    implementation(Dependencies.composeUi)
+    implementation(Dependencies.composeRuntime)
+    implementation(Dependencies.composeUiGraphics)
+    implementation(Dependencies.composeMaterial)
+    debugImplementation(Dependencies.composeUiToolingPreview)
+    implementation(Dependencies.composeUiTooling)
+
 }
+
+fun DependencyHandler.hilt() {
+    implementation(Dependencies.hiltAndroid)
+    kapt(Dependencies.hiltCompiler)
+}
+
+fun DependencyHandler.retrofit() {
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.moshiConverter)
+    implementation(Dependencies.okhttp)
+    implementation(Dependencies.okhttpLoggingInterceptor)
+}
+
+fun DependencyHandler.room() {
+    implementation(Dependencies.roomRuntime)
+    implementation(Dependencies.roomKtx)
+    kapt(Dependencies.roomCompiler)
+}
+
+
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,4 +2,8 @@ object Versions {
     const val kotlin = "1.9.0"
     const val compose = "1.5.0"
     const val hilt = "2.48"
+    const val composeMaterial3 = "1.2.0"
+    const val okHttp = "4.10.0"
+    const val retrofit = "2.9.0"
+    const val room = "2.5.2"
 }

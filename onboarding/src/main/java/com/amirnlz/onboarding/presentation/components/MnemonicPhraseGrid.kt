058d280a8742ffe9989dbe390dfc6bc7bc0361a3
Amir <amiirnlz79@gmail.com>
2024-12-11 20:28:43 +0330

    Feat: change ui of mnemonic word showing

--- onboarding/src/main/java/com/amirnlz/onboarding/presentation/components/MnemonicPhraseGrid.kt
+++ onboarding/src/main/java/com/amirnlz/onboarding/presentation/components/MnemonicPhraseGrid.kt
@@ -1,36 +1,33 @@
 package com.amirnlz.onboarding.presentation.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.amirnlz.onboarding.domain.model.MnemonicPhrase
+import com.amirnlz.onboarding.presentation.model.MnemonicWordUiModel
 
 
 @Composable
-fun MnemonicPhraseGrid(modifier: Modifier = Modifier, mnemonicPhrase: MnemonicPhrase) {
-
-
+fun MnemonicPhraseGrid(
+    modifier: Modifier = Modifier, mnemonicPhrase: MnemonicPhrase
+) {
+    val mnemonicWords = remember(mnemonicPhrase) {
+        mnemonicPhrase.words.mapIndexed { index, word ->
+            MnemonicWordUiModel(word, index)
+        }
+    }
 
     LazyVerticalGrid(
         modifier = modifier,
@@ -39,64 +36,44 @@ fun MnemonicPhraseGrid(modifier: Modifier = Modifier, mnemonicPhrase: MnemonicPh
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(mnemonicPhrase.words.withIndex().toList()) { (index, word) ->
-            MnemonicWordItem(word = word, index = index)
+        items(items = mnemonicWords, key = { it.index }) { wordModel ->
+            AnimatedVisibility(
+                visible = true,
+                enter = fadeIn(animationSpec = tween(durationMillis = 300)),
+                exit = fadeOut(animationSpec = tween(300)) + shrinkVertically()
+            ) {
+                MnemonicWordItem(
+                    wordModel = wordModel, modifier = Modifier.animateItem()
+                )
+            }
         }
-
     }
 }
 
 
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF,
+)
 @Composable
-fun MnemonicWordItem(
-    modifier: Modifier = Modifier,
-    word: String,
-    index: Int,
-) {
-    Row(
-        modifier = modifier
-            .clip(RoundedCornerShape(8.dp))
-            .border(1.dp, MaterialTheme.colorScheme.surfaceDim, RoundedCornerShape(8.dp))
-            .background(MaterialTheme.colorScheme.surfaceVariant),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(
-            modifier = Modifier
-                .padding(horizontal = 12.dp, vertical = 8.dp)
-                .width(20.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = (index + 1).toString(),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-        }
-
-        Box(
-            modifier = Modifier
-                .width(1.dp)
-                .height(24.dp)
-                .background(MaterialTheme.colorScheme.surfaceDim)
+private fun MnemonicPhraseGridPreview() {
+    val previewMnemonicPhrase = MnemonicPhrase(
+        words = listOf(
+            "apple",
+            "banana",
+            "cherry",
+            "date",
+            "elderberry",
+            "fig",
+            "grape",
+            "honeydew",
+            "kiwi",
+            "lemon",
+            "mango",
+            "nectarine"
         )
+    )
 
-        Box(
-            modifier = Modifier
-                .padding(horizontal = 12.dp, vertical = 8.dp)
-        ) {
-            Text(
-                text = word,
-                fontSize = 14.sp,
-                color = Color.Black
-            )
-        }
-    }
+    MnemonicPhraseGrid(mnemonicPhrase = previewMnemonicPhrase)
 }
 
-@Preview
-@Composable
-private fun MnemonicWordItemPreview() {
-    MnemonicWordItem(word = "Banana", index = 1)
-    
-}

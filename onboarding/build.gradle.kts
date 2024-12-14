2ec519489cf5a368124790e06997e52c25646b55
Amir <amiirnlz79@gmail.com>
2024-12-14 23:38:24 +0330

    Chore(onboarding): use buildSrc dependencies

--- onboarding/build.gradle.kts
+++ onboarding/build.gradle.kts
@@ -9,9 +9,8 @@ android {
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro") // Valid in library modules
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -35,24 +34,12 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.material3)
-    implementation(Dependencies.composeUi)
-    implementation(libs.androidx.compose.ui.ui.tooling.preview)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.material.icons.core)
-    implementation(libs.androidx.material.icons.extended)
+    core()
+    compose()
+    icons()
     implementation(project(":app"))
-
-    debugImplementation(libs.androidx.compose.ui.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
-
     testImplementation(libs.junit)
-
     androidTestImplementation(libs.ui.test.junit4)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
